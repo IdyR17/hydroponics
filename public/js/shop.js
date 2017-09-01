d1b02bed5b6205fe2d2560d12e0ce50e3fe9b460
Juri <idyr17@jurhidy.com>
2017-09-01 14:40:51 -0400

    guides placeholders

--- public/js/shop.js
+++ public/js/shop.js
@@ -1,3 +1,13 @@
+var li_item=function(image,){ `<li>
+<a href="images/g5.jpg" rel="" class="b-link-stripe b-animate-go  thickbox" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.">
+
+    <img src="images/g5.jpg" alt="" />
+    <div>
+        <h5>GrowItIn</h5>
+        <span>Sed ut perspiciatis unde omnis iste natus error sit</span>
+    </div>
+</a>
+</li>`}
 const CONTENT= `			  <div class="gallery w3layouts-agile" id="gallery">
 <div class="container">
     <div class="gallery-top wthee-agileinfo">
@@ -5,95 +15,9 @@ const CONTENT= `			  <div class="gallery w3layouts-agile" id="gallery">
     </div>
         <section>
             <ul id="da-thumbs" class="da-thumbs">
-                <li>
-                    <a href="images/g1.jpg" rel="" class="b-link-stripe b-animate-go  thickbox" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.">
-                        <img src="images/g1.jpg" alt="" />
-                        <div>
-                            <h5>GrowItIn</h5>
-                            <span>Sed ut perspiciatis unde omnis iste natus error sit</span>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="images/g2.jpg" rel="" class="b-link-stripe b-animate-go  thickbox" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.">
-                        <img src="images/g2.jpg" alt="" />
-                        <div>
-                            <h5>GrowItIn</h5>
-                            <span>Sed ut perspiciatis unde omnis iste natus error sit</span>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="images/g3.jpg" rel="" class="b-link-stripe b-animate-go  thickbox" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.">
-                    
-                        <img src="images/g3.jpg" alt="" />
-                        <div>
-                            <h5>GrowItIn</h5>
-                            <span>Sed ut perspiciatis unde omnis iste natus error sit</span>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="images/g4.jpg" rel="" class="b-link-stripe b-animate-go  thickbox" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.">
-                
-                        <img src="images/g4.jpg" alt="" />
-                        <div>
-                            <h5>GrowItIn</h5>
-                            <span>Sed ut perspiciatis unde omnis iste natus error sit</span>
-                        </div>
-                    </a>
-                </li>
-                <li>	
-                    <a href="images/g6.jpg" rel="" class="b-link-stripe b-animate-go  thickbox" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.">
-                    
-                        <img src="images/g6.jpg" alt="" />
-                        <div>
-                            <h5>GrowItIn</h5>
-                            <span>Sed ut perspiciatis unde omnis iste natus error sit</span>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="images/g9.jpg" rel="" class="b-link-stripe b-animate-go  thickbox" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.">
-                        
-                        <img src="images/g9.jpg" alt="" />
-                        <div>
-                            <h5>GrowItIn</h5>
-                            <span>Sed ut perspiciatis unde omnis iste natus error sit</span>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="images/g8.jpg" rel="" class="b-link-stripe b-animate-go  thickbox" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.">
-                        
-                        <img src="images/g8.jpg" alt="" />
-                        <div>
-                            <h5>GrowItIn</h5>
-                            <span>Sed ut perspiciatis unde omnis iste natus error sit</span>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="images/g7.jpg" rel="" class="b-link-stripe b-animate-go  thickbox" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.">
-                        
-                        <img src="images/g7.jpg" alt="" />
-                        <div>
-                            <h5>GrowItIn</h5>
-                            <span>Sed ut perspiciatis unde omnis iste natus error sit</span>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="images/g5.jpg" rel="" class="b-link-stripe b-animate-go  thickbox" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.">
-                    
-                        <img src="images/g5.jpg" alt="" />
-                        <div>
-                            <h5>GrowItIn</h5>
-                            <span>Sed ut perspiciatis unde omnis iste natus error sit</span>
-                        </div>
-                    </a>
-                </li>
+                ${li_item}
             </ul>
         </section>
 </div>
 </div>`;
+
